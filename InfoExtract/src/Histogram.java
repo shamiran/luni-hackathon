f7b786688606b9b8b2266f78d6a471c9e5124558
matlarsson <mathildalarsson@outlook.com>
2017-11-07 21:15:05 +0100

    updates to histogram

--- InfoExtract/src/Histogram.java
+++ InfoExtract/src/Histogram.java
@@ -15,7 +15,39 @@ public class Histogram {
 	}
 	
 	private void readExam(Exam exam){
-		String[] listOfExams = exam.getString().split(" ");
+		String string = exam.getString();
+		string.toLowerCase();
+		
+		string.replace(".","");
+		string.replace(",","");
+		string.replace("!","");
+		string.replace("?","");
+		string.replace("0","");
+		string.replace("1","");
+		string.replace("2","");
+		string.replace("3","");
+		string.replace("4","");
+		string.replace("5","");
+		string.replace("6","");
+		string.replace("7","");
+		string.replace("8","");
+		string.replace("9","");
+		string.replace("0","");
+		string.replace(":","");
+		string.replace(";","");
+		string.replace("+","");
+		string.replace("-","");
+		string.replace("a)","");
+		string.replace("b)","");
+		string.replace("c)","");
+		string.replace("d)","");
+		string.replace("e)","");
+		string.replace("f)","");
+		string.replace("(","");
+		string.replace(")","");
+		
+		
+		String[] listOfExams = string.split(" ");
 		for(String word: listOfExams){
 			
 			
