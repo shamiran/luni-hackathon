5804f9d33f3f5940d90bd1437a75a0985449a84c
Holmgreen <gustav__holmgren@hotmail.com>
2017-11-07 21:00:49 +0100

    Dummy commit

--- InfoExtract/src/HelloWorld.java
+++ InfoExtract/src/HelloWorld.java
@@ -1,9 +1,13 @@
+import java.util.ArrayList;
+
 public class HelloWorld {
 	public static void main(String[] args) {
 		String infile = "PDF/EDA16";
 		String outfile = "data/histogram_eda16.txt";
 		Exam exam = new Exam(infile);
 		Histogram hist = new Histogram(exam);
+		ArrayList<String> exercises = getExcercises(exam.getString());
 		TextFileConverter converter = new TextFileConverter(hist, outfile);
 	}
+		
 }
